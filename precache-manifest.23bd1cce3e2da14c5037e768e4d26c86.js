73448fdf715b8cdcc3e023f5ccb8c701a9795246
Daniil-Kudlach <daniil.kudlach2018@gmail.com>
2020-02-26 12:20:39 +0200

    Updates

---
+++ precache-manifest.23bd1cce3e2da14c5037e768e4d26c86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb52fce36f4ad729af08be1888cb69c4",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "57a31a69428865039b9f",
+    "url": "/todo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "559063b174c417bf93d6",
+    "url": "/todo/static/js/2.b79ca5bf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todo/static/js/2.b79ca5bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57a31a69428865039b9f",
+    "url": "/todo/static/js/main.6ea89cb5.chunk.js"
+  },
+  {
+    "revision": "79683bb16114007ae43b",
+    "url": "/todo/static/js/runtime-main.2cb95132.js"
+  }
+]);
